e54ceaa872686ffc5a2a7e6272bc2e3997e4d682
Gabriel Pereira Faravola <gabrielfaravola@gmail.com>
2025-08-25 12:07:10 +0000

    Update

---
+++ Lab03/PipeExample.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define VECTOR_SIZE 1000
+#define NUM_PROCESSES 4
+
+void Soma_Parcial(int start, int end, double* vector){
+    int soma = 0;
+    for(int i = start; i < end; i++){
+        soma = soma + vector[i];
+    }
+}
+
+int main(){
+    double *vector = (double *) malloc(VECTOR_SIZE * sizeof(double));
+    for (int i = 0; i < VECTOR_SIZE; i++) vector[i] = (double) i;
+
+    int pipefd[2];
+    pid_t pid;
+
+    int chunk_size = VECTOR_SIZE / NUM_PROCESSES;
+
+    for(int i = 0; i < NUM_PROCESSES; i++){
+        int start = i * chunk_size;
+        int end = start + chunk_size;
+
+
+        
+    }
+
+}
