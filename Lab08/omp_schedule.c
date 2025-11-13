25b9a15377c80e25e360072f98334c2e1526182b
Gabriel Pereira Faravola <gabrielfaravola@gmail.com>
2025-11-13 11:46:48 +0000

    OpenMP Schdule implementation

---
+++ Lab08/omp_schedule.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+#include <unistd.h> // para usleep
+
+#define N 16
+#define CHUNK_SIZE 2
+
+int main () {
+
+    printf ("---Testando schedule (static, %d) ---\n", CHUNK_SIZE);
+    #pragma omp parallel for schedule (static , CHUNK_SIZE )
+    for (int i = 0; i < N; ++i){
+        // Simula uma carga de trabalho que aumenta com ’i’
+        usleep (( i % 4) * 10000) ;
+        printf ("Thread %d processou a iteracao %d\n", omp_get_thread_num(), i);
+    }
+
+    printf ("\n--- Testando schedule (dynamic, %d) ---\n", CHUNK_SIZE);
+    #pragma omp parallel for schedule (dynamic, CHUNK_SIZE)
+    for (int i = 0; i < N; ++i){
+        usleep ((i % 4) * 10000) ;
+        printf ("Thread %d processou a iteracao %d\n", omp_get_thread_num(), i);
+    }
+
+    printf ("\n--- Testando schedule (guided) ---\n");
+    #pragma omp parallel for schedule (guided)
+    for (int i = 0; i < N; ++ i){
+        usleep ((i % 4) * 10000) ;
+        printf ("Thread %d processou a iteracao %d\n", omp_get_thread_num(), i);
+    }
+
+    printf ("\n--- Testando schedule (auto) ---\n");
+    #pragma omp parallel for schedule (auto)
+    for (int i = 0; i < N; ++ i){
+        usleep ((i % 4) * 10000) ;
+        printf ("Thread %d processou a iteracao %d\n", omp_get_thread_num(), i);
+    }
+    return 0;
+}
